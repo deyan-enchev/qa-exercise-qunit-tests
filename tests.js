4a43048e59f14c1ad5a632bac706cc1d59bf0269
deyan-enchev <deyanenchev@abv.bg>
2015-05-19 17:46:15 +0300

    Update tests.js

--- tests.js
+++ tests.js
@@ -30,3 +30,51 @@ test('When I click save with empty fields, I should get an error', function() {
     start();
   }, 1000);
 });
+
+// Basically a check more like the first one - state of the first element
+test('Check the first element', function () {
+	ok($("#RiskTitle", iframe.contents()).is(":visible"), "RiskTitle element is seen");
+});
+
+// Checking the drop down - state of the second element
+test('Check the second element', function () {
+	ok($("#status", iframe.contents()).is(":visible"), "Status drop down is seen");
+});
+
+// Checking the approval date - state of the third element
+test('Check the third element', function () {
+	ok($("#approvalDate", iframe.contents()).is(":visible"), "Approval date is seen");
+});
+
+// Checkiing the owner - state of the fourth element
+test('Check the fourth element', function () {
+	ok($("#owner", iframe.contents()).is(":visible"), "Owner is seen");
+});
+
+// Checking the button - state of the submit element
+test('Check the button element', function () {
+	ok($(".btn.btn-primary", iframe.contents()).is(":visible"), "Button is seen");
+});
+
+// Checking how the element will handle text input.
+test('Check how the "risk title" element handles text insert', function (assert) {
+	var iframe = $('#myFrame').contents();
+	iframe.find("#RiskTitle").val("SampleText");
+	assert.equal("SampleText", iframe.find("#RiskTitle").val(), "RiskTitle element handles insert");
+});
+
+// Checking how the element will handle date input. This works well in IE, but fails in Chrome I believe I am missing something here, and will be glad to know what :)
+test('Check how the "approval date" element handles date insert', function (assert) {
+	var iframe = $('#myFrame').contents();
+	iframe.find("#approvalDate").val("1984/03/16");
+	assert.deepEqual("1984/03/16", iframe.find("#approvalDate").val(), "Approval Date element handles insert");
+});
+
+// Checking the drop down for proper values - array of objects
+test('When you check the drop down items', function (assert) {
+	var options = $('#myFrame').contents().find("#status option");
+	assert.equal('Open', options[0].innerHTML);
+	assert.equal('Closed', $(options[1]).html());
+	assert.equal('Rejected', $(options[2]).html());
+	assert.equal('Approved', $(options[3]).html());
+});
